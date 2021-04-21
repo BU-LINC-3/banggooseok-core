ccd286c317d91f53a38828a30af182bae3a4ed6c
Novang <qkdxorjs1002@naver.com>
2021-04-21 14:02:36 +0900

    DB: Refactor VO name

--- src/main/java/kr/banggooseok/database/vo/RoomsSimpleVO.java
+++ src/main/java/kr/banggooseok/database/vo/RoomsSimpleVO.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
-public class RoomsVO {
+public class RoomsSimpleVO {
 
     @SerializedName("id")
     private int id;
